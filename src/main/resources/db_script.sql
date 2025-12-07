c0aa86b9a5a41823bbea911f3ef8d71b805a41c0
Blagovest Kirilov <blagovestkirilov1@gmail.com>
2025-12-07 19:28:17 +0200

    Fix db script

--- src/main/resources/db_script.sql
+++ src/main/resources/db_script.sql
@@ -1,22 +1,22 @@
-CREATE DATABASE car_rental;
+CREATE DATABASE CAR_RENTAL;
 
-CREATE TABLE cars
+CREATE TABLE CARS
 (
-    id            INT IDENTITY (1,1) PRIMARY KEY,
-    maker         NVARCHAR(100)  NOT NULL,
-    model         NVARCHAR(100)  NOT NULL,
-    year          INT            NOT NULL,
-    color         NVARCHAR(50)   NOT NULL,
-    license_plate NVARCHAR(50)   NOT NULL,
-    daily_price   DECIMAL(10, 2) NOT NULL,
-    is_rented     BIT            NOT NULL DEFAULT 0
+    ID            INT IDENTITY (1,1) PRIMARY KEY,
+    MAKER         NVARCHAR(100)  NOT NULL,
+    MODEL         NVARCHAR(100)  NOT NULL,
+    YEAR          INT            NOT NULL,
+    COLOR         NVARCHAR(50)   NOT NULL,
+    LICENSE_PLATE NVARCHAR(50)   NOT NULL,
+    DAILY_PRICE   DECIMAL(10, 2) NOT NULL,
+    IS_RENTED     BIT            NOT NULL DEFAULT 0
 );
 
-INSERT INTO Cars (maker, model, year, color, license_plate, daily_price, is_rented)
+INSERT INTO CARS (MAKER, MODEL, YEAR, COLOR, LICENSE_PLATE, DAILY_PRICE, IS_RENTED)
 VALUES ('VOLKSWAGEN', 'GOLF 5', 2008, 'SILVER', 'KH1234AB', 100.00, 0);
 
-INSERT INTO Cars (maker, model, year, color, license_plate, daily_price, is_rented)
+INSERT INTO CARS (MAKER, MODEL, YEAR, COLOR, LICENSE_PLATE, DAILY_PRICE, IS_RENTED)
 VALUES ('BMW', 'X5', 2022, 'BLACK', 'CB5678CK', 90.00, 0);
 
-INSERT INTO Cars (maker, model, year, color, license_plate, daily_price, is_rented)
+INSERT INTO CARS (MAKER, MODEL, YEAR, COLOR, LICENSE_PLATE, DAILY_PRICE, IS_RENTED)
 VALUES ('FORD', 'FOCUS', 2019, 'BLUE', 'CA9012EP', 40.00, 1);
